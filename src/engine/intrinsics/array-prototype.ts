6ac07e1451e67add72510da2b1b811cea3677ed1
Dirk de Visser <github@dirkdevisser.nl>
2026-02-08 16:39:07 +0100

    feat: array push and join

--- src/engine/intrinsics/array-prototype.ts
+++ src/engine/intrinsics/array-prototype.ts
@@ -1,11 +1,21 @@
 import { ArrayExoticMethods } from "../abstract-operations/array-exotic.ts";
+import { createBuiltinFunction } from "../abstract-operations/built-in-function-object.ts";
 import {
 	definePropertyOrThrow,
+	get,
+	lengthOfArrayLike,
 	makeBasicObject,
+	set,
 } from "../abstract-operations/object-operations.ts";
 import { OrdinaryObjectInternalMethods } from "../abstract-operations/ordinary-object.ts";
 import { PropertyDescriptor } from "../abstract-operations/property-map.ts";
+import { toObject, toString } from "../abstract-operations/type-conversion.ts";
 import type { Realm } from "../execution-contexts/realm.ts";
+import {
+	normalCompletion,
+	throwCompletion,
+	unwrapCompletion,
+} from "../types-and-values/completion-record.ts";
 import { EngineValue } from "../types-and-values/data-types.ts";
 import type { ObjectInternalSlots } from "../types-and-values/data-types.ts";
 
@@ -45,6 +55,114 @@ export function intrinsicArrayPrototype(realm: Realm) {
 			"constructor",
 			new PropertyDescriptor({
 				value: realm.intrinsics["%Array%"]!,
+				writable: true,
+				enumerable: false,
+				configurable: true,
+			}),
+		);
+
+		// https://tc39.es/ecma262/multipage/indexed-collections.html#sec-array.prototype.join
+		definePropertyOrThrow(
+			arrayPrototype,
+			"join",
+			new PropertyDescriptor({
+				value: createBuiltinFunction(
+					(thisArgument, argumentsList, _newTarget) => {
+						const o = toObject(thisArgument!);
+						if (o.type === "throw") {
+							return o;
+						}
+
+						const len = lengthOfArrayLike(o.value);
+						if (len.type === "throw") {
+							return len;
+						}
+
+						let seperator = argumentsList[0];
+						if (!seperator || seperator.isUndefined()) {
+							seperator = EngineValue.string(",");
+						} else {
+							seperator = unwrapCompletion(toString(seperator));
+						}
+
+						let r = "";
+						let k = 0;
+						while (k < len.value) {
+							if (k > 0) {
+								r += seperator.asString().data.value;
+							}
+
+							const element = get(o.value, `${k}`);
+							if (element.type === "throw") {
+								return element;
+							}
+
+							if (element.value.isUndefined() || element.value.isNull()) {
+								++k;
+								continue;
+							}
+
+							const s = toString(element.value);
+							if (s.type === "throw") {
+								return s;
+							}
+
+							r += s.value.asString().data.value;
+
+							++k;
+						}
+
+						return normalCompletion(EngineValue.string(r));
+					},
+					1,
+					"join",
+					[],
+					realm,
+				),
+				writable: true,
+				enumerable: false,
+				configurable: true,
+			}),
+		);
+
+		// https://tc39.es/ecma262/multipage/indexed-collections.html#sec-array.prototype.push
+		definePropertyOrThrow(
+			arrayPrototype,
+			"push",
+			new PropertyDescriptor({
+				value: createBuiltinFunction(
+					(thisArgument, argumentsList, _newTarget) => {
+						const o = toObject(thisArgument!);
+						if (o.type === "throw") {
+							return o;
+						}
+
+						const len = lengthOfArrayLike(o.value);
+						if (len.type === "throw") {
+							return len;
+						}
+
+						const argCount = argumentsList.length;
+						if (len.value + argCount > Math.pow(2, 53) - 1) {
+							return throwCompletion(
+								new TypeError("Array.prototype.push: length overflow"),
+							);
+						}
+
+						for (const e of argumentsList) {
+							set(o.value, `${len.value}`, e, true);
+							len.value += 1;
+						}
+
+						set(o.value, "length", EngineValue.number(len.value), true);
+
+						return normalCompletion(EngineValue.number(len.value));
+					},
+					1,
+					"push",
+					[],
+					realm,
+				),
 				writable: true,
 				enumerable: false,
 				configurable: true,
