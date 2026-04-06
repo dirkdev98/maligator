20bc6c60f6a92d9423ccd6f2e968098c4dc35fcb
Dirk de Visser <github@dirkdevisser.nl>
2026-04-06 13:59:27 +0200

    feat(ir): compile used functions
    We handle simple params for now. We also add instructions to all blocks that don't terminate themselves.
    
    We do some basic checks to see if a function is used before compiling it in.

--- src/ir.ts
+++ src/ir.ts
@@ -38,11 +38,15 @@ export interface IntermediateProgram {
 	 * - captured: captured variables.
 	 * - global: top-level declared variables.
 	 */
-	bindingToStorage: Map<
+	bindingToStorage: Map<Binding, BindingLocation>;
+
+	/**
+	 * Keep track of which functions we compiled already.
+	 */
+	bindingToFunctionCache: Map<
 		Binding,
 		{
-			type: "local" | "captured" | "global";
-			index: number;
+			fnIndex: number;
 		}
 	>;
 
@@ -52,8 +56,21 @@ export interface IntermediateProgram {
 	nextGlobalIndex: number;
 }
 
+type BindingLocation =
+	| {
+			type: "local" | "global";
+			index: number;
+	  }
+	| {
+			type: "captured";
+			functionIndex: number;
+			index: number;
+	  };
+
 export interface IRFunction {
 	semanticFile: SemanticFile;
+	functionIndex: number;
+
 	blocks: Array<IRBlock>;
 
 	/**
@@ -106,7 +123,7 @@ export type IRInstruction =
 			blocks: [number];
 	  }
 	| {
-			type: "loadNumber";
+			type: "createNumber";
 
 			// [destination]
 			registers: [number];
@@ -114,25 +131,37 @@ export type IRInstruction =
 			value: number;
 	  }
 	| {
-			type: "loadUndefined";
+			type: "createUndefined";
 
 			// [destination]
 			registers: [number];
+	  }
+	| {
+			type: "createFunction";
+
+			// [destination]
+			registers: [number];
+
+			functionIndex: number;
 	  }
 	| {
 			type: `load${"Local" | "Captured" | "Global"}`;
 
 			// [destination]
 			registers: [number];
+
+			functionIndex?: number;
 			index: number;
 	  }
 	| {
-			// TODO(opt): there is an optimization oppurtunity when a store is 'immediately' followed by
-			//  a load.
+			// TODO(opt): there is an optimization opportunity when a store is 'immediately'
+			// followed by a load.
 			type: `store${"Local" | "Captured" | "Global"}`;
 
 			// [source]
 			registers: [number];
+
+			functionIndex?: number;
 			index: number;
 	  }
 	| {
@@ -181,6 +210,7 @@ export function compileSemanticProgramToIr(semantic: SemanticProgram) {
 
 		compiledModuleInitForPaths: new Set(),
 		bindingToStorage: new Map(),
+		bindingToFunctionCache: new Map(),
 
 		nextGlobalIndex: 0,
 	};
@@ -212,6 +242,7 @@ function compileFileInit(program: IntermediateProgram, initFile: SemanticFile) {
 
 	const fn: IRFunction = {
 		semanticFile: initFile,
+		functionIndex: program.functions.length,
 		blocks: [],
 
 		parameterCount: 0,
@@ -220,10 +251,146 @@ function compileFileInit(program: IntermediateProgram, initFile: SemanticFile) {
 		nextCapturedIndex: 0,
 	};
 
-	const idx = program.functions.push(fn) - 1;
-	compileStatementsToBlock(program, fn, initFile.ast.body);
+	// TODO: We trace collect these at the moment, so initialization order is incorrect.
+	program.functions.push(fn);
 
-	return idx;
+	compileStatementsToBlock(program, fn, initFile.ast.body);
+	endFunction(fn);
+
+	return fn.functionIndex;
+}
+
+function compileNewFunction(
+	program: IntermediateProgram,
+	binding: Binding,
+	functionNode: ESTree.Node,
+) {
+	if (
+		functionNode.type !== "FunctionDeclaration" &&
+		functionNode.type !== "FunctionExpression" &&
+		functionNode.type !== "ArrowFunctionExpression"
+	) {
+		return -1;
+	}
+
+	if (program.bindingToFunctionCache.has(binding)) {
+		return program.bindingToFunctionCache.get(binding)!.fnIndex;
+	}
+
+	// Brute-force find the file. We should do the linkup earlier, so we have / know which file
+	// it is.
+	let foundFile: SemanticFile | undefined = undefined;
+	for (const file of program.semantic.files) {
+		if (file.nodeToBinding.get(functionNode)) {
+			foundFile = file;
+			compileFileInit(program, foundFile);
+			break;
+		}
+	}
+
+	const fn: IRFunction = {
+		semanticFile: foundFile ?? program.semantic.files[0]!,
+		functionIndex: program.functions.length,
+		blocks: [],
+
+		parameterCount: functionNode.params.length,
+		nextRegisterDestination: 0,
+		nextLocalIndex: 0,
+		nextCapturedIndex: 0,
+	};
+
+	program.functions.push(fn);
+	program.bindingToFunctionCache.set(binding, { fnIndex: fn.functionIndex });
+
+	compileFunctionParams(program, fn, functionNode);
+	compileStatementsToBlock(
+		program,
+		fn,
+		functionNode.body?.type === "BlockStatement"
+			? normalizeStatementOrBlock(functionNode.body)
+			: functionNode.body // Handle auto-returning from single-line arrow functions.
+				? normalizeStatementOrBlock({
+						type: "ReturnStatement",
+						argument: functionNode.body,
+					})
+				: [],
+	);
+
+	endFunction(fn);
+
+	return fn.functionIndex;
+}
+
+/**
+ * Return 'undefined' from all blocks that don't unconditionally jump yet.
+ */
+function endFunction(fn: IRFunction) {
+	for (const block of fn.blocks) {
+		// TODO(opt): once optimized we can probably do with scanning the whole block, since there
+		//  might be earlier returns happening.
+		const lastInstruction = block.instructions.at(-1);
+		if (lastInstruction?.type === "return" || lastInstruction?.type === "jump") {
+			continue;
+		}
+
+		const destinationRegister = nextRegisterDestination(fn);
+		block.instructions.push(
+			{
+				type: "createUndefined",
+				registers: [destinationRegister],
+			},
+			{
+				type: "return",
+				registers: [destinationRegister],
+			},
+		);
+	}
+}
+
+function compileFunctionParams(
+	program: IntermediateProgram,
+	fn: IRFunction,
+	node:
+		| ESTree.FunctionDeclaration
+		| ESTree.FunctionExpression
+		| ESTree.ArrowFunctionExpression,
+) {
+	const block: IRBlock = {
+		instructions: [],
+	};
+	fn.blocks.push(block);
+
+	for (const param of node.params) {
+		// Always allocate a register for the parameter. So we kinda silently skip unsupported
+		// params for now.
+		const sourceRegister = nextRegisterDestination(fn);
+
+		switch (param.type) {
+			case "ArrayPattern":
+				break;
+			case "AssignmentPattern":
+				break;
+			case "RestElement":
+				break;
+			case "ObjectPattern":
+				break;
+			case "Identifier": {
+				const binding = fn.semanticFile.nodeToBinding.get(param);
+				if (!binding) {
+					throw new Error(`No binding found for parameter ${param.name}`);
+				}
+				const location = getOrCreateBindingLocation(program, fn, binding);
+				storeRegisterAtLocation(block, location, sourceRegister);
+				break;
+			}
+		}
+	}
+
+	// Always jump to the next block unconditionally. This will be the function body.
+	block.instructions.push({
+		type: "jump",
+		blocks: [1],
+	});
 }
 
 /**
@@ -252,7 +419,7 @@ function compileStatementsToBlock(
 			//
 			// ```
 			// BLOCK:
-			//   loadNumber 1 in reg1
+			//   createNumber 1 in reg1
 			//   jumpIf reg1 BLOCK2
 			// BLOCK2:
 			//   call somefn
@@ -284,6 +451,10 @@ function compileStatementsToBlock(
 				compileExpressionStatement(program, fn, block, statement);
 				break;
 			}
+			case "FunctionDeclaration": {
+				compileFunctionDeclaration(program, fn, block, statement);
+				break;
+			}
 			case "IfStatement": {
 				compileIfStatement(program, fn, block, statement);
 				break;
@@ -307,8 +478,43 @@ function compileExpressionStatement(
 	compileExpression(program, fn, block, statement.expression);
 }
 
+function compileFunctionDeclaration(
+	program: IntermediateProgram,
+	fn: IRFunction,
+	block: IRBlock,
+	statement: ESTree.FunctionDeclaration,
+) {
+	const binding = fn.semanticFile.nodeToBinding.get(statement);
+	if (!binding) {
+		return;
+	}
+
+	if (
+		binding.usageNodes.length === 0 ||
+		(binding.usageNodes.length === 1 &&
+			(binding.usageNodes[0] === statement || binding.usageNodes[0] === statement.id))
+	) {
+		// Function is only used in its declaration, so we can skip it.
+		return;
+	}
+
+	const fnIndex = compileNewFunction(program, binding, statement);
+	const location = getOrCreateBindingLocation(program, fn, binding);
+
+	const destination = nextRegisterDestination(fn);
+	block.instructions.push({
+		type: "createFunction",
+		registers: [destination],
+
+		functionIndex: fnIndex,
+	});
+
+	storeRegisterAtLocation(block, location, destination);
+}
+
 /**
- * Compile an if statement, reading the condition and jumping to the consequent or alternate block.
+ * Compile an if statement, reading the condition and jumping to the consequent or alternate
+ * block.
  */
 function compileIfStatement(
 	program: IntermediateProgram,
@@ -328,17 +534,20 @@ function compileIfStatement(
 		blocks: [consequentBlock],
 	});
 
-	if (statement.alternate) {
-		const alternateBlock = compileStatementsToBlock(
-			program,
-			fn,
-			normalizeStatementOrBlock(statement.alternate),
-		);
-		block.instructions.push({
-			type: "jump",
-			blocks: [alternateBlock],
-		});
-	}
+	// We always create an alternate block so we have something to resume.
+	const alternate = statement.alternate ?? {
+		type: "BlockStatement",
+		body: [],
+	};
+	const alternateBlock = compileStatementsToBlock(
+		program,
+		fn,
+		normalizeStatementOrBlock(alternate),
+	);
+	block.instructions.push({
+		type: "jump",
+		blocks: [alternateBlock],
+	});
 }
 
 /**
@@ -366,32 +575,7 @@ function compileVariableDeclaration(
 		}
 
 		const location = getOrCreateBindingLocation(program, fn, binding);
-		switch (location.type) {
-			case "local": {
-				block.instructions.push({
-					type: "storeLocal",
-					registers: [source],
-					index: location.index,
-				});
-				break;
-			}
-			case "global": {
-				block.instructions.push({
-					type: "storeGlobal",
-					registers: [source],
-					index: location.index,
-				});
-				break;
-			}
-			case "captured": {
-				block.instructions.push({
-					type: "storeCaptured",
-					registers: [source],
-					index: location.index,
-				});
-				break;
-			}
-		}
+		storeRegisterAtLocation(block, location, source);
 	}
 }
 
@@ -458,7 +642,7 @@ function compileIdentifier(
 	if (identifier.name === "undefined") {
 		const destination = nextRegisterDestination(fn);
 		block.instructions.push({
-			type: "loadUndefined",
+			type: "createUndefined",
 			registers: [destination],
 		});
 
@@ -469,8 +653,8 @@ function compileIdentifier(
 	if (!binding) {
 		return -1;
 	}
-	const location = getOrCreateBindingLocation(program, fn, binding);
 
+	const location = getOrCreateBindingLocation(program, fn, binding);
 	const destination = nextRegisterDestination(fn);
 	switch (location.type) {
 		case "local": {
@@ -493,6 +677,8 @@ function compileIdentifier(
 			block.instructions.push({
 				type: "loadCaptured",
 				registers: [destination],
+
+				functionIndex: location.functionIndex,
 				index: location.index,
 			});
 			break;
@@ -511,7 +697,7 @@ function compileLiteral(
 	if (typeof literal.value === "number" && Number.isInteger(literal.value)) {
 		const destination = nextRegisterDestination(fn);
 		block.instructions.push({
-			type: "loadNumber",
+			type: "createNumber",
 			registers: [destination],
 
 			value: literal.value,
@@ -556,6 +742,7 @@ function getOrCreateBindingLocation(
 			case "captured": {
 				location = {
 					type: "captured",
+					functionIndex: fn.functionIndex,
 					index: fn.nextCapturedIndex++,
 				};
 				break;
@@ -577,6 +764,44 @@ function getOrCreateBindingLocation(
 	}
 
 	return location;
+}
+
+/**
+ * Store register at a binding location.
+ */
+function storeRegisterAtLocation(
+	block: IRBlock,
+	location: BindingLocation,
+	register: number,
+) {
+	switch (location.type) {
+		case "local": {
+			block.instructions.push({
+				type: "storeLocal",
+				registers: [register],
+				index: location.index,
+			});
+			break;
+		}
+		case "global": {
+			block.instructions.push({
+				type: "storeGlobal",
+				registers: [register],
+				index: location.index,
+			});
+			break;
+		}
+		case "captured": {
+			block.instructions.push({
+				type: "storeCaptured",
+				registers: [register],
+
+				functionIndex: location.functionIndex,
+				index: location.index,
+			});
+			break;
+		}
+	}
 }
 
 /**
